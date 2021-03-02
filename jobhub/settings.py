01c786f504fc8849582dbfbb71fe9844198b9172
Gagan Hang Limbu <toshiro.limbu01@gmail.com>
2021-03-02 20:46:15 +0545

    migrate

--- jobhub/settings.py
+++ jobhub/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+
+    # apps
+    'accounts.apps.AccountsConfig',
+    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
